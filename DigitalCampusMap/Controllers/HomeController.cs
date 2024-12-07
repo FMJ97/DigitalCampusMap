210d968e2c4b7c7b58262b5341aebae549c2a2e9
Fran Mihovil Jukic <corpfmj@gmail.com>
2024-12-07 09:06:14 +0100

    Store Submitted Form Data

--- DigitalCampusMap/Controllers/HomeController.cs
+++ DigitalCampusMap/Controllers/HomeController.cs
@@ -1,16 +1,52 @@
 using DigitalCampusMap.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace DigitalCampusMap.Controllers
 {
     public class HomeController : Controller
     {
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly DigitalCampusMapContext _context;
+
+        public HomeController(DigitalCampusMapContext context, ILogger<HomeController> logger)
         {
+            _context = context;
             _logger = logger;
+        }
+       
+
+        [HttpPost]
+        public IActionResult SubmitContactForm(Contactform model)
+        {
+            if (ModelState.IsValid)
+            {
+                var contact = new Contactform
+                {
+                    Email = model.Email,
+                    Subject = model.Subject,
+                    Description = model.Description,
+                    CreatedAt = DateTime.Now 
+                };
+
+                _context.Contactforms.Add(contact);
+                _context.SaveChanges();  
+
+                return Content("Contact form submitted successfully!");
+            }
+
+            return View(model);
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
         public IActionResult Index()
@@ -21,19 +57,6 @@ namespace DigitalCampusMap.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [HttpPost]
-        public IActionResult SubmitContactForm()
-        {
-
-            return Content("Good");
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- DigitalCampusMap/Program.cs
+++ DigitalCampusMap/Program.cs
@@ -1,7 +1,13 @@
+using DigitalCampusMap.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DigitalCampusMapContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
